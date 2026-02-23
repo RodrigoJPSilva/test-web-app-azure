5ff7b1805ad14e844743a3312882bed5cde9fc3d
RodrigoJPSilva <rodrigojrpsilva>
2026-02-23 11:13:24 -0300

    Feat: added restcontroller

--- src/main/java/com/java21/testapp/controller/TestController.java
+++ src/main/java/com/java21/testapp/controller/TestController.java
@@ -2,7 +2,9 @@ package com.java21.testapp.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class TestController {
 
     @GetMapping
